05e572cf5705053d02312d086c1728523e107f6e
Saw96x <964488739@qq.com>
2020-05-24 21:23:23 +0800

    Improve MergeSort and add QuickSort.

--- src/MyReality/Ep2/QuickSort.java
+++ src/MyReality/Ep2/QuickSort.java
@@ -4,44 +4,11 @@ import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Scanner;
 
-public class MergeSort
+public class QuickSort
 {
-
     public static void sort(Comparable[] a)
     {
-        Comparable[] aux = new Comparable[a.length];
-        sort(a, 0, a.length - 1, aux);
-    }
 
-    private static void merge(Comparable[] a, int lo, int mid, int hi, Comparable[] aux)
-    {
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++)
-        {
-            aux[k] = a[k];
-        }
-        for (int k = lo; k <= hi; k++)
-        {
-            if (i > mid)
-                a[k] = aux[j++];
-            else if (j > hi)
-                a[k] = aux[i++];
-            else if (less(aux[j], aux[i]))
-                a[k] = aux[j++];
-            else
-                a[k] = aux[i++];
-        }
-    }
-
-    private static void sort(Comparable[] a, int lo, int hi, Comparable[] aux)
-    {
-        if (hi <= lo)
-            return;
-        int mid = lo + (hi - lo) / 2;
-        sort(a, lo, mid, aux);
-        sort(a, mid + 1, hi, aux);
-        if (!(less(a[mid], a[mid + 1]) || a[mid].equals(a[mid + 1])))
-            merge(a, lo, mid, hi, aux);
     }
 
     private static boolean less(Comparable v, Comparable t)
