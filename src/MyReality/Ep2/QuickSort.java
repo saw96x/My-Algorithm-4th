bd07fee3b90d0c172d9dec340ea5e74586583d34
Saw96x <964488739@qq.com>
2020-05-25 08:23:55 +0800

    Finish QuickSort

--- src/MyReality/Ep2/QuickSort.java
+++ src/MyReality/Ep2/QuickSort.java
@@ -1,6 +1,7 @@
 package MyReality.Ep2;
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Scanner;
 
@@ -8,7 +9,37 @@ public class QuickSort
 {
     public static void sort(Comparable[] a)
     {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
 
+    private static void sort(Comparable[] a, int lo, int hi)
+    {
+        if (lo >= hi)
+            return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j+1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi)
+    {
+        int i = lo, j = hi + 1;
+        Comparable v = a[lo];
+        while (true)
+        {
+            while (less(a[++i], v))
+                if (i == hi)
+                    break;
+            while (less(v, a[--j]))
+                if (j == lo)
+                    break;
+            if (i >= j)
+                break;
+            swap(a, i, j);
+        }
+        swap(a, lo, j);
+        return j;
     }
 
     private static boolean less(Comparable v, Comparable t)
